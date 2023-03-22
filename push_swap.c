062db0fbbaf401e645badf49df1106c1945eba21
Ahmed Bajaou <ahbajaou@e2r5p10.1337.ma>
2023-03-22 02:55:11 +0000

    push_swap

--- push_swap.c
+++ push_swap.c
@@ -6,37 +6,40 @@
 /*   By: ahbajaou <ahbajaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 23:28:53 by ahbajaou          #+#    #+#             */
-/*   Updated: 2023/03/22 00:53:28 by ahbajaou         ###   ########.fr       */
+/*   Updated: 2023/03/22 02:28:13 by ahbajaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft/libft.h"
-void    ft_error(void)
+
+void ft_error(void)
 {
-    write(1,"Error\n",6);
+    write(1, "Error\n", 6);
+    exit(0);
 }
 
-int main(int ac,char **av)
+int main(int ac, char **av)
 {
     // int i = 0;
-    int cnt = 1;
-    t_push *pu;
-    (void)av;
-    pu = malloc(sizeof(t_push));
     if (ac < 2)
         ft_error();
-    else
-        while (cnt < ac)
-        {
-            while (pu->tmp)
-            {
-                pu->push = ft_strjoin(pu->push,pu->tmp);
-                pu->tmp = av[cnt];
-            }
-            free(pu->tmp);
-            printf("%s\n",pu->push);
-            cnt++;
-        }
+    int cnt = 1;
+    char **tmp;
+    t_push *pu;
+
+    tmp = malloc(sizeof(char *) * ac);
+    tmp[ac] = NULL;
+    pu = malloc(sizeof(t_push));
+    int i = 0;
+    while (tmp[i])
+    {
+        tmp[i] = av[i + 1];
+        pu->push = ft_strjoin(pu->push, tmp);
+        printf("%s\n", tmp);
+        i++;
+    }
+    // free(pu->tmp);
+    // printf("%s\n",pu->push);
     return (0);
 }
