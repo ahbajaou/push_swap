871968bdde3622c4505f8ac57237e2d670e79fc5
Ahmed Bajaou <ahbajaou@e2r5p12.1337.ma>
2023-04-12 17:53:49 +0000

    pushswap

--- instructions/instructions.c
+++ instructions/instructions.c
@@ -6,7 +6,7 @@
 /*   By: ahbajaou <ahbajaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 09:16:46 by ahbajaou          #+#    #+#             */
-/*   Updated: 2023/04/12 07:24:50 by ahbajaou         ###   ########.fr       */
+/*   Updated: 2023/04/12 16:17:59 by ahbajaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,77 +16,84 @@
 void rotate_a(node **stack_a)
 {
 
-    if (*stack_a == NULL || (*stack_a)->next == NULL)
-        return;
-    node *tmp = *stack_a;
-    while (tmp->next != NULL){
-        tmp = tmp->next;
-    }
-    tmp->next = (*stack_a);
-    *stack_a = (*stack_a)->next;
-    tmp->next->next = NULL;
+	if (*stack_a == NULL || (*stack_a)->next == NULL)
+		return;
+	node *tmp = *stack_a;
+	while (tmp->next != NULL){
+		tmp = tmp->next;
+	}
+	write(1,"ra\n",3);
+	tmp->next = (*stack_a);
+	(*stack_a) = (*stack_a)->next;
+	tmp->next->next = NULL;
 }
 
 void    swap_a(node **stack_a)
 {
-    if (*stack_a != NULL && (*stack_a)->next != NULL)
-    {
-        int     tmp;
-        node *first = (*stack_a);
-        node *second = (*stack_a)->next;
+	if (*stack_a != NULL && (*stack_a)->next != NULL)
+	{
+		int     tmp;
+		node *first = (*stack_a);
+		node *second = (*stack_a)->next;
 
-        tmp = first->data;
-        first->data = second->data;
-        second->data = tmp;
-        (*stack_a) = first;
-        // write(1,"sa\n",3);
-    }
-    return ;
+		tmp = first->data;
+		first->data = second->data;
+		second->data = tmp;
+		(*stack_a) = second;
+		(*stack_a)->next = first;
+		(*stack_a)->next->next = NULL;
+		write(1,"sa\n",3);
+	}
 } 
 void  push_b(node **stack_a,node **stack_b)
 {
-    // if ((*stack_a) == NULL || (*stack_a)->next == NULL)
-    //     return ;
-    node *first;
-    first = (*stack_a);
-    // printf("%d\n",first->data);  
-    // if ((*stack_b) == NULL)
-    (*stack_b) = creat_node(first->data);
-        // add_font(&(*stack_b),creat_node(first->data));
+		node *first;
+		first = (*stack_a);
+	// {
+	if ((*stack_a) != NULL)
+			add_font(&(*stack_b),creat_node(first->data));
+	else
+		(*stack_b) = creat_node(first->data);
+	write(1,"pb\n",3);
+
 }
 void  push_a(node **stack_a,node **stack_b)
 {
-    if ((*stack_b) == NULL)
-        return ;
-    node *first;
-    first = (*stack_b);
-    add_font(&(*stack_a),creat_node(first->data));
-    // (*stack_a) = creat_node(first->data);
+	if ((*stack_b) == NULL)
+		return ;
+	node *first;
+	first = (*stack_b);
+	add_font(&(*stack_a),creat_node(first->data));
+	write(1,"pa\n",3);
+	// (*stack_a) = creat_node(first->data);
 }
 
 void    swap_b(node **stack_b)
 {
-    if ((*stack_b) == NULL || (*stack_b)->next == NULL)
-        return ;
-    int tmp;
-    node *first = (*stack_b);
-    node *second = (*stack_b)->next;
-    tmp = first->data;
-    first->data = second->data;
-    second->data = tmp;
-
-    (*stack_b) = first;
+	if ((*stack_b) == NULL || (*stack_b)->next == NULL)
+		return ;
+	int tmp;
+	node *first = (*stack_b);
+	node *second = (*stack_b)->next;
+	tmp = first->data;
+	first->data = second->data;
+	second->data = tmp;
+	(*stack_b) = second;
+	(*stack_b)->next = first;
+	(*stack_b)->next->next = NULL;
+	write(1,"sb\n",3);
 }
 
 void    rotate_b(node **stack_b)
 {
-    if ((*stack_b) == NULL)
-        return ;
-    node *tmp = (*stack_b);
-    while (tmp->next != NULL)
-            tmp = tmp->next;
-        tmp->next = (*stack_b);
-        (*stack_b) = (*stack_b)->next;
-        tmp->next->next = NULL;
+	if ((*stack_b) == NULL)
+		return ;
+	node *tmp = (*stack_b);
+	while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = (*stack_b);
+		(*stack_b) = (*stack_b)->next;
+		tmp->next->next = NULL;
+		write(1,"rb\n",3);
 }
 
--- sort_five_orless/smal_sort.c
+++ sort_five_orless/smal_sort.c
@@ -6,7 +6,7 @@
 /*   By: ahbajaou <ahbajaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 05:17:24 by ahbajaou          #+#    #+#             */
-/*   Updated: 2023/04/12 07:29:59 by ahbajaou         ###   ########.fr       */
+/*   Updated: 2023/04/12 16:17:37 by ahbajaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,78 +15,78 @@
 
 void    sort_two(node **stack_a)
 {
-    //    if ((*stack_a) == NULL)
-    //         return ;
-    node *first = (*stack_a);
-    node *second = (*stack_a)->next;
-        if (first->data > second->data)
-            rotate_a(stack_a);
-        // print_list((*stack_a));
+
+	node *first = (*stack_a);
+	node *second = (*stack_a)->next;
+	if (first->data < second->data)
+		swap_a(stack_a);
 }
 void    sort_three(node **stack_a, node **stack_b)
 {
-    node *first;
-    node *seceond;
-    node *three; 
+	node *first;
+	node *seceond;
+	node *three; 
+	(void)stack_b;
+	first = (*stack_a);
+	seceond = (*stack_a)->next;
+	three = (*stack_a)->next->next;
 
-    first = (*stack_a);
-    seceond = (*stack_a)->next;
-    three = (*stack_a)->next->next;
-
-    node *smaller;
-    if (first->data < seceond->data && first->data < three->data)
-        smaller = first;
-    else if (seceond->data < first->data && seceond->data < three->data)
-         smaller = seceond;
-    else
-         smaller = three;
-    push_b(&smaller,stack_b);
-    free_stack(stack_a,smaller->data);
-    sort_two(stack_a);
-    push_a(stack_a,stack_b);
+	node *smaller;
+	if (first->data < seceond->data && first->data < three->data)
+		smaller = first;
+	else if (seceond->data < first->data && seceond->data < three->data)
+		 smaller = seceond;
+	else
+		 smaller = three;
+	push_b(&smaller,stack_b);
+	free_stack(stack_a,smaller->data);
+	sort_two(stack_a);
+	push_a(stack_a,stack_b);
 }
 void    sort_4(node **stack_a,node **stack_b)
 {
-    (void)stack_b;
-    node *tmp = (*stack_a);
-    node *one = tmp;
-    node *two = tmp->next;
-    node *three = tmp->next->next;
-    node *four = tmp->next->next->next;
+	(void)stack_b;
+	node *tmp = (*stack_a);
+	node *one = tmp;
+	node *two = tmp->next;
+	node *three = tmp->next->next;
+	node *four = tmp->next->next->next;
 
-    node *smaller;
-    smaller = one;
-    if (two->data < smaller->data)
-    {
-        smaller = two;
-    }
-    if (three->data < smaller->data)
-    {
-        smaller = three;
-    }
-    if (four->data < smaller->data)
-    {
-        smaller = four;
-    }
-    push_b(&smaller,stack_b);
-    sort_three(stack_a,stack_b);
-    free_stack(stack_a,smaller->data);
-    push_a(stack_a,stack_b);
-    free_stack(stack_b,smaller->data);
+	node *smaller;
+	smaller = one;
+	if (two->data < smaller->data)
+		smaller = two;
+	if (three->data < smaller->data)
+		smaller = three;
+	if (four->data < smaller->data)
+		smaller = four;
+	push_b(&smaller,stack_b);
+	free_stack(stack_a,smaller->data);
+	sort_three(stack_a,stack_b);
+	push_a(stack_a,&(*stack_b)->next);
+	free_stack(stack_b,smaller->data);
+}
+void    sort_five(node **stack_a,node **stack_b)
+{
+	
 }
 void    sort_five_or_less(node **stack_a,node **stack_b ,int ac)
 {
-    if (ac == 3)
-    {
-        sort_two(stack_a);
-    }
-    else if (ac == 4)
-    {
-        sort_three(stack_a,stack_b);
-        free_stack(stack_b,(*stack_b)->data);
-    }
-    else if (ac == 5)
-    {
-        sort_4(stack_a,stack_b);
-    }
+	if (ac == 3)
+	{
+		sort_two(stack_a);
+	}
+	else if (ac == 4)
+	{
+		sort_three(stack_a,stack_b);
+		free_stack(stack_b,(*stack_b)->data);
+	}
+	else if (ac == 5)
+	{
+		sort_4(stack_a,stack_b);
+	}
+	else if (ac == 6)
+	{
+	    sort_five(stack_a,stack_b);
+	}
 }
